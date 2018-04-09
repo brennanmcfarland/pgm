e9d371b85e9eaf97f14e4766acae25f3cca9876e
Brennan McFarland <4brothersnosisters@gmail.com>
2018-04-09 11:22:54 -0400

    at least now everythings the right dimension and it doesnt throw an error

--- gmmlib.py
+++ gmmlib.py
@@ -8,29 +8,41 @@ import numpy as np
 # gmm = [{'mean': mu[m], 'covariance': sigma[m], 'prior': 1.0/ngmm} for m in range(ngmm)]
 def expectation(data, gmm):
     # sigma = [m['covariance'] for m in gmm] # an array
-    p_n_k = [m['prior'] for m in gmm]
+    print(gmm[0]['prior'])
+    p_n_k = []
+    p_n_k.append(gmm[0]['prior'])
+    p_n_k.append(gmm[1]['prior'])
+    # p_n_k = [m['prior'] for m in gmm]
     x = data
     p_k = np.sum(p_n_k)
-    print(p_n_k) # one array of size 2
-    print(x) # a whole lotta arrays of size 2
-    print(p_k) # a number, 1, apparently?
-    u0 = [np.sum(p_n_k[0]*x[n]/p_k) for n in range(len(data))]
-    u1 = [np.sum(p_n_k[1]*x[n]/p_k) for n in range(len(data))]
-    u = [u0, u1]
+    #print(p_n_k) # one array of size 2
+    #print(x) # a whole lotta arrays of size 2
+    #print(p_k) # a number, 1, apparently?
+    u = []
+    for k in range(len(data[0])):
+        u0 = np.sum([p_n_k[0]*x[n][k]/p_k for n in range(len(data))])
+        u1 = np.sum([p_n_k[1]*x[n][k]/p_k for n in range(len(data))])
+        u.append([u0, u1])
     return u
 
 
 # update the mean, covariance, and class prior for each class?
 def maximization(posterior, data, gmm):
-    maximization_mean(posterior, data, gmm)
+    return maximization_mean(posterior, data, gmm)
     # n['covariance'] = covariance[n] # TODO: actually update the covarianceu0 = [np.sum(p_n_k[0]*x[n]/p_k) for n in range(len(data))]
 
 
 def maximization_mean(posterior, data, gmm):
+    print("old gmm: ", gmm)
+    print("posterior: ", posterior)
     # update mean
     mean = expectation(data, gmm)
     for n in range(len(gmm)):
         gmm[n]['mean'] = mean[n]
+    print("new mean: ", mean)
     # update class prior
     for n in range(len(gmm)):
-        gmm[n]['prior'] = posterior[n]
+        gmm[n]['prior'] = posterior[n][0] # TODO: this isn't right, why is posterior a nested array?
+    print("new prior: ", posterior)
+    print("new gmm: ", gmm)
+    return gmm
