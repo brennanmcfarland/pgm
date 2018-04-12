08aa5c78fdfd8f9a0dfd46981b78edada09224df
Brennan McFarland <4brothersnosisters@gmail.com>
2018-04-11 20:39:30 -0400

    everything seems to work except maximization, but also needs cleanup and explanation

--- gmmlib.py
+++ gmmlib.py
@@ -13,13 +13,19 @@ from sklearn import datasets
 # gmm = [{'mean': mu[m], 'covariance': sigma[m], 'prior': 1.0/ngmm} for m in range(ngmm)]
 def expectation(data, gmm):
     x = data
-    # new textbook page 425
-    # scipy multivariate normal (the denominator part factors out - normalizing constant is also sum of gaussians)
     posteriors = []
     for n in range(len(data)):
         posteriors.append([])
         for k in range(len(gmm)):
-            p_k_xn = multivariate_normal.pdf(x[n], mean=gmm[k]['mean'], cov=gmm[k]['covariance']) # what to do about determinants?  do those cancel too?
+            p_k_xn = 0
+            try:
+                p_k_xn = multivariate_normal.pdf(x[n], mean=gmm[k]['mean'], cov=gmm[k]['covariance']) # what to do about determinants?  do those cancel too?
+            except:
+                print()
+                print("xn:",x[n])
+                print("mean:",gmm[k]['mean'])
+                print("covar:",gmm[k]['covariance'])
+                print("prior:",gmm[k]['prior'])
             p_k_xn *= gmm[k]['prior']
             posteriors[n].append(p_k_xn)
         normalizing_constant = np.sum(posteriors[n])
@@ -33,49 +39,47 @@ def expectation(data, gmm):
 
 
 # update the mean, covariance, and class prior for each class
-def maximization(posterior, data, gmm):
-    print(gmm)
+def maximization(posterior, data, oldgmm):
+    gmm = copy.deepcopy(oldgmm)
+    #print(gmm)
     x = data
     gmm = maximization_mean(posterior, data, gmm)
-    # NOTE: may be reusing k for both number of classes and dimensions, elsewhere too?
-    # there's 3 levels of nesting: which covariance matrix itself (k),
-    # the dimension of the datapoints (d),
-    # and the dimension of the covariance (c)
-    # but how to get the individual vals of the covariance matrix?
     covariances = []
+    priors = []
+    # TODO: the means are right, but the priors and covariances
+    # are both way smaller than they should be
     for k in range(len(gmm)):
-        covariances_k = []
-        for d in range(len(data[0])):
-            #covariance = 0
-            covariance_0 = [0,0]
-            covariance_1 = [0,0]
-            for n in range(len(data)):
-                # covariance += multivariate_normal.pdf(x[n], mean=gmm[k]['mean'], cov=gmm[k]['covariance'])
-                covariance_0 = [c + () for c in covariance_0]
-                covariance_1 = [c + () for c in covariance_1]
-            covariances_k.append(covariance)
-        # TODO: this is not right, just copying the values, but why is there that extra level of nesting?
-        # of course this doesn't even work for testing because the matrix is not invertible
-        cov_tmp = []
-        cov_tmp.append(copy.deepcopy(covariances_k))
-        cov_tmp.append(copy.deepcopy(covariances_k))
-        #gmm[k]['covariance'] = np.array(cov_tmp)
-        gmm[k]['covariance'] = np.array(copy.deepcopy(covariances_k))
-        covariances.append(covariances_k)
-    print(covariances)
-    print(gmm)
-    # TODO: actually update covariance and class prior
+        #covariances_k = np.empty(len(data))
+#        covariances_k = np.zeros_like(gmm[0]['covariance'])
+        covariance = np.zeros_like(gmm[k]['covariance'])#0
+        prior = 0
+        for n in range(len(data)):
+            p_n_k = posterior[n][k]
+            p_k = np.sum(posterior[n])
+            prior += p_n_k/p_k
+
+        for n in range(len(data)):
+            p_n_k = posterior[n][k]
+            p_k = np.sum(posterior[n])
+            mat_row = np.reshape(x[n] - gmm[k]['mean'], (2,1))
+            covariance += p_n_k * (mat_row * mat_row.transpose())
+
+        print("unnormalized prior: ", prior)
+        prior /= len(x)
+        gmm[k]['covariance'] = covariance
+        covariances.append(covariance)
+        gmm[k]['prior'] = prior
+        priors.append(prior)
     return gmm
-    # n['covariance'] = covariance[n] # 0 = [np.sum(p_n_k[0]*x[n]/p_k) for n in range(len(data))]
 
 
 # update just the mean for each class
 # u[k] = Σ[n] p[n,k]x^(n)/p[k]
-def maximization_mean(posterior, data, gmm):
+def maximization_mean(posterior, data, oldgmm):
+    gmm = copy.deepcopy(oldgmm)
     means = []
     for k in range(len(gmm)):
-        # means.append[]
-        mean = 0
+        mean = np.zeros_like(gmm[k]['mean'])
         p_k = 0
         for n in range(len(data)):
             x_n = data[n]
@@ -84,17 +88,17 @@ def maximization_mean(posterior, data, gmm):
             mean += p_n_k*x_n
         mean /= p_k
         means.append(mean)
-        gmm[k]['mean'] = copy.deepcopy(mean)
+        gmm[k]['mean'] = mean
     # print(means)
     return gmm
 
 
 # using sklearn's pca functionality
 def dimReducePCA(data, newdimensions):
-    pca = decomposition.PCA(n_components=2)
+    pca = decomposition.PCA(n_components=newdimensions) # TODO: change to newdimensions?
     pca.fit(data)
-    data = pca.transform(data)
-    #TODO: needs to return reduced data, eigenvectors and eigenvalues
-    # data is data components is eigenvectors, and explained variance is eigenvalues (I think?)
-    print(pca.components_)
-    return data, pca.components_, pca.explained_variance_
+    data -= np.mean(data, axis=0)
+    covariance = np.matmul(data.transpose(), data)/data.shape[0]
+    ev, ew = np.linalg.eig(covariance)
+    pcs = pca.transform(data)
+    return pcs, ew, ev
